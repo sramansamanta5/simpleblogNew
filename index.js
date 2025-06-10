ff11f98a526279feaf452fdc6b95a47422284303
sramansamanta5 <sramansamanta2023@gmail.com>
2025-06-10 14:59:45 +0530

    set up basic backend

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import mongoose from 'mongoose';
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config();
+const app = express();
+
+
+app.use(cors())
+const PORT=process.env.Port || 5000
+
+
+mongoose.connect(process.env.Mongo_URI)
+.then(()=>{console.log("Database is connected..")})
+.catch((err)=>{console.log(err)})
+
+
+app.get('/',(req,res)=>{
+    res.send('Home page running...')
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Webapp is running on PORT ...`)
+})
